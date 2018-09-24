03d15553bf83f78c9ea211350d5bbf42a4c07e9f
m.seiji <web.works.ms@gmail.com>
2018-09-24 20:03:07 +0900

    change_contacts_mailaddress

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,21 +1,22 @@
 class ContactMailer < ApplicationMailer
-  default form: "web.works.ms@gmail.com"
+  #default form: "web.works.ms@gmail.com"
+  default form: "from_vampire_rose@yahoo.co.jp"
 
   def contact_mail(user)
     @user = user  
-    mail(:to => "web.works.ms@gmail.com",
+    mail(:to => "from_vampire_rose@yahoo.co.jp",
     :subject => "ホームページからお問い合わせがありました")
   end
 
   def main_contact_mail(user)
     @user = user
-     mail(:to => "web.works.ms@gmail.com",
+     mail(:to => "from_vampire_rose@yahoo.co.jp",
     :subject => "ホームページからお問い合わせがありました")
   end
 
   def item_contact_mail(user)
     @user = user
-     mail(:to => "web.works.ms@gmail.com",
+    mail(:to => "vampirerose1017@gmail.com",
     :subject => "ホームページからお問い合わせがありました")
   end
 end
