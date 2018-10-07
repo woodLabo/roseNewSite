adf365672308143999ecd40429a4999670880cf1
m.seiji <web.works.ms@gmail.com>
2018-10-08 00:53:24 +0900

    actice_admin_user_set

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'web.works.ms@gmail.com', password: 'seiji0828', password_confirmation: 'seiji0828') if Rails.env.development?
+#AdminUser.create!(email: 'web.works.ms@gmail.com', password: 'seiji0828', password_confirmation: 'seiji0828') if Rails.env.development?
+AdminUser.create!(email: 'web.works.ms@gmail.com', password: 'seiji0828', password_confirmation: 'seiji0828')
