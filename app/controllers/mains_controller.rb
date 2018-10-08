76759f63bf783680abb7befab5787a12a78fe02d
m.seiji <web.works.ms@gmail.com>
2018-10-08 23:27:29 +0900

    main_news_order_desc

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,7 +1,7 @@
 require 'date'
 class MainsController < ApplicationController
   def index
-    @news = News.all.first
+    @news = News.all.order("id DESC").first
     @lives = Schedule.all.where(types: "0").where("DATE(date) >= '#{Date.today}'").order("date ASC").first
   end
 
